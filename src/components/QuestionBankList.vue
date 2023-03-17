7a488fae9dc5ead7feabd680796aee4cef175bfe
J.sky <bosichong@qq.com>
2023-03-17 20:33:35 +0800

    优化首页列表

--- src/components/QuestionBankList.vue
+++ src/components/QuestionBankList.vue
@@ -2,15 +2,13 @@
 
   <div v-for="item in navigation"  class="content">
     <n-h2><n-text type="primary">{{ item.title }}</n-text></n-h2>
-    <n-grid :x-gap="8" :y-gap="14" cols="1 400:2 600:3 800:4 1000:5 1200:6">
+    <n-grid :x-gap="8" :y-gap="14" cols="1 500:2 750:3 1000:4 1200:5">
       <n-grid-item v-for="it in item.items">
-        <div class="qbl">
           <router-link :to="it.url">
             <n-card :title="it.title" hoverable>
               {{ it.description }}
             </n-card>
           </router-link>
-        </div>
       </n-grid-item>
     </n-grid>
   </div>
@@ -36,7 +34,4 @@ axios.get("/data/navigation.json").then((response)=>{
 
 <style scoped>
 
-.qbl {
-  height: 108px;
-}
 </style>
