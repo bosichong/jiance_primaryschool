b79ffc633fb106f81a4f40cf326fc25d40818dd0
J.sky <bosichong@qq.com>
2023-03-20 18:52:58 +0800

    更新展示

--- src/components/QuestionBankList.vue
+++ src/components/QuestionBankList.vue
@@ -22,7 +22,7 @@ import axios from "axios";
 
 const navigation = ref([])
 
-axios.get("/data/nlist.json").then((response)=>{
+axios.get("/data/nl.json").then((response)=>{
   navigation.value = response.data
   // console.log(navigation.value)
 })
