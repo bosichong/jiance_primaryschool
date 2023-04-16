f0fb5404fc6e5c78d25fb1c9e4b13d3b59f6f855
J.sky <bosichong@qq.com>
2023-04-16 22:48:31 +0800

    更新演示页面https://suiyan.cc/demo/jiance/

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import naive from 'naive-ui'
 import './assets/main.css'
 import store from "@/store";
+import axios from "axios";
+
+axios.defaults.baseURL="https://suiyan.cc/demo/jiance/" //设置默认网址
 
 const app = createApp(App)
 app.use(naive)
